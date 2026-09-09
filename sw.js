c52787d1509f007ac3c10d570d7cb883395f6ef2
ranjitb1084 <54815334+ranjitb1084@users.noreply.github.com>
2026-09-08 20:20:49 -0400

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='attendance-tracker-v5';
+const CACHE='attendance-tracker-v6';
 const ASSETS=['./','./index.html','./manifest.webmanifest','./icon-192.png','./icon-512.png'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
 self.addEventListener('activate',e=>e.waitUntil(self.clients.claim()));
